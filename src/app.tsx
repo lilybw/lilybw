c63699fdce2c0acd58127326712bfaf142c92adc
lilybw <lilybwanscher@gmail.com>
2025-10-31 21:36:30 +0100

    something

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,13 @@
 import { createEffect, createSignal, onCleanup } from "solid-js";
 import "./app.css";
 import { CSSProperty } from "./util/animationUtil";
-import { RepeatingSlider } from "./components/RepeatingSlider";
+import { MenuBar } from "./components/MenuBar";
 
 export default function App() {
 
   createEffect(() => {
     const clearSiteBreath = CSSProperty({name: "--site-breath-driver", target: document.documentElement})
-      .oscillate(0, 2, 0.25);
+      .oscillate(-1, 1, 0.25);
 
     const clearSiteElapsedSeconds = CSSProperty({name: "--site-elapsed-seconds", target: document.documentElement})
       .asFunctionOfElapsedTimeS((elapsedS) => elapsedS);
@@ -18,12 +18,12 @@ export default function App() {
     })
   })
 
-  const siteBackground = <div class="site-background"></div>;
 
   return (
     <main>
-      <RepeatingSlider durationS={30} paneContent={siteBackground} parentStyle={{height: "100vh", position: "absolute", top: 0, left: 0}}/>
-      <h1 class="page-title-purple">Hello world!</h1>
+      <div class="site-background"></div>
+      <h1 class="page-title-aqua-marine">ABSOLUTELY NOT KDA</h1>
+      <MenuBar />
     </main>
   );
 }
---
+++ src/components/MenuBar.tsx
@@ -0,0 +1,16 @@
+import { JSX } from "solid-js";
+import "./MenuBar.css";
+
+interface MenuBarProps {}
+
+export function MenuBar(props: MenuBarProps) {
+  return (
+    <div class="menu-bar">
+        <div style={{"display": "flex", "gap": ".5rem", "flex-direction": "row"}}>
+            <h2 class="menu-bar-button">Projects</h2>
+            <h2 class="menu-bar-button">Projects</h2>
+            <h2 class="menu-bar-button">Projects</h2>
+        </div>
+    </div>
+  );
+}
