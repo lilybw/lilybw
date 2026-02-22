d687b7ce0f20246f3106891d19f38b1e0319876f
lilybw <lilybwanscher@gmail.com>
2026-02-22 09:32:15 +0100

    converging

--- src/util/arrayUtil.ts
+++ src/util/arrayUtil.ts
@@ -3,10 +3,3 @@ export const appendMutatedCopy = <T>(arr: T[], mutator: (t: T) => T) => {
     return [...arr, ...arr.map(mutator)];
 }
 
-export const convergeToArray = <T>(e: undefined | null | T | T[]): T[] => {
-    if (!e || e === null) {
-        return [];
-    }
-
-    return Array.isArray(e) ? e : [e];
-}
--- src/util/convergeUtil.ts
+++ src/util/convergeUtil.ts
@@ -5,3 +5,13 @@ export const convergeToArray = <T>(e: undefined | null | T | T[]): T[] => {
 
     return Array.isArray(e) ? e : [e];
 }
+
+export const convergeToValue = <T>(e: undefined | null | T | (() => T), defaultValue?: T): T => {
+    if (!e || e === null) return defaultValue as T;
+
+    if (typeof e === 'function') {
+        return (e as (() => T))();
+    }
+
+    return e;
+}
